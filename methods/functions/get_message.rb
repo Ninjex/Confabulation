59b77c931adf46cd18de6667ff2bcea257b7e9ee
Ninjex <Ninjex@localhost.localdomain>
2014-02-10 02:01:26 -0500

    Moving / Removing files

--- methods/functions/get_message.rb
+++ methods/functions/get_message.rb
@@ -3,25 +3,4 @@ def get_message
   p text
   message = text.join(' ')
   message.strip
-=begin
-  yaml_words = File.read('defined_words.yml')
-  defined_words = YAML.load(yaml_words)
-
-  text.each do |check_word| # Each word from the user
-    defined_words.each do |word, defi| # The words and definitions
-      #new_word = word #"%"+word # Add % to the word to check
-      if word == check_word
-        # Use bang! here to change it in place
-        message.gsub!("#{check_word}", "#{defi}")
-      end
-    end
-
-  end
-=end
-  # Be very careful here: strip! will return nil if nothing is stripped
-  # message = message.strip! # Remove the trailing whitespace
-  # Don't need return in ruby
-  # return message # Return the variable
-  
-  #message.strip
 end
