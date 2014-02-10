c8527c39e5ed530ec3b21a486b03b74d90aef384
Ninjex <Ninjex@localhost.localdomain>
2014-02-10 02:03:06 -0500

    Removes

--- methods/chan_send.rb
+++
@@ -1,4 +0,0 @@
-def chan_send(text)
-  if @channel.downcase == @nick.downcase then send_data("PRIVMSG #{@user_name_first} :> #{text}")
-  else send_data("PRIVMSG #{@channel} :> #{text}") end
-end
--- methods/get_message.rb
+++
@@ -1,6 +0,0 @@
-def get_message
-  text = @input.drop(4) # Grab all data following a command
-  p text
-  message = text.join(' ')
-  message.strip
-end
