7076aaeb1b2bc042547c2c7c8fdb6d2e97b3f17f
Ninjex <Ninjex@localhost.localdomain>
2014-02-10 02:15:36 -0500

    Remove excess code

--- methods/load_methods.rb
+++ methods/load_methods.rb
@@ -1,9 +1,6 @@
 @@standard_command_array = [] # Create an array for regular user commands
 @@mod_command_array = [] # Create an array for moderator commands
 @@admin_command_array = [] # Create an array for administrator commands
-@@regular_commands = 'Regular Commands: '
-@@mod_commands = 'Moderator Commands: '
-@@admin_commands = 'Administrator Commands: '
 
 METHODS   = './methods'
 ACCESS    = '/access/'
@@ -11,7 +8,6 @@ FUNCTIONS = '/functions/'
 access_paths = ["standard/", "mod/", "admin/"]
 def parse_access(path)
   Dir["#{METHODS}#{ACCESS}#{path}*.rb"].each do |file|
-    puts "#{file}"
     require file
     func = file.gsub("#{METHODS}#{ACCESS}#{path}", "")[0..-4]
     if path == "standard/" then @@standard_command_array.push(":./" + func) end
